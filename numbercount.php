e43ce9da6122c9add4f54fecaff82669a7175072
jayesh10 <jayesh.mali@somaiya.edu>
2016-11-30 05:04:02 +0530

    Updated numbercount.php

--- numbercount.php
+++ numbercount.php
@@ -5,6 +5,6 @@ $num2 = array(1,2,3,4,5,6,7,8,9);
 foreach($num1 as $value1)
   foreach($num2 as $value2)
 	$value3 = $value1;  
-var_dump(json_encode($value3));
+print_r(json_encode($value3));
 
 ?>
