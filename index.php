720402064950e570c9dc39eaa19344c597323565
jayesh10 <jayesh.mali@somaiya.edu>
2016-11-30 01:12:55 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
   </HEAD>
 <BODY>
   <SECTION>
+  <form method="post">
   		Name:&nbsp&nbsp&nbsp&nbsp<INPUT type="TEXT" id="name"><br>
   		Comment:&nbsp<INPUT type="TEXT" id="comment"><br>
          <BUTTON Tpye="submit" id="add" >Add The Comment</BUTTON>
+   </form>
   </SECTION>
 </BODY>
 </HTML>
+          <?php 
+          		if(!isset[])
 
