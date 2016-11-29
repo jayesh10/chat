ee72376485e5f78a2a7f45a5285822b56eaff7b5
jayesh10 <jayesh.mali@somaiya.edu>
2016-11-30 01:10:44 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </HEAD>
 <BODY>
   <SECTION>
-  		Name:&nbsp<INPUT type="TEXT" id="name"><br>
+  		Name:&nbsp&nbsp&nbsp&nbsp<INPUT type="TEXT" id="name"><br>
   		Comment:&nbsp<INPUT type="TEXT" id="comment"><br>
          <BUTTON Tpye="submit" id="add" >Add The Comment</BUTTON>
   </SECTION>
