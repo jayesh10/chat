7368233d276b46c76f6841ea0853b93cf3823cac
jayesh10 <jayesh.mali@somaiya.edu>
2016-11-30 05:00:58 +0530

    Updated numbercount.php

--- numbercount.php
+++ numbercount.php
@@ -3,6 +3,8 @@ $num1 = array(1,2,3,4,5,6,7,8,9);
 $num2 = array(1,2,3,4,5,6,7,8,9);
 
 foreach($num1 as $value1)
-  var_dump($value1);
+  foreach($num2 as $value2)
+  
+print_r($value1,$value2);
 
 ?>
