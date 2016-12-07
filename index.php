380f063795b45a619e40303d5d0eee631fc6b202
jayesh10 <jayesh.mali@somaiya.edu>
2016-12-07 21:19:45 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -2,24 +2,12 @@
   <HEAD>
   	<TITLE>HOME</TITLE>
   </HEAD>
-<BODY>
-  <?php 
-    $name=$comment="";
-	
-        if($_SERVER[REQUEST_METHOD]=="POST")
-          if(empty($_POST["name]"))
-        	$namerr="Name is required!";
-			else
-            $name = $_POST["name"];
-		if(empty())
-?>
-  <SECTION>
+<BODY> 
   <form >
   		Name:<INPUT type="TEXT" name="name"><br>
   		Comment:<INPUT type="TEXT" comment="comment"><br>
          <INPUT Tpye="submit"  >
    </form>
-  </SECTION>
+  
 </BODY>
 </HTML>
-          
