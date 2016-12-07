077820d6a9af0d0afecb9232d4ffea723a094d43
jayesh10 <jayesh.mali@somaiya.edu>
2016-12-07 22:19:34 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <form>
  	<div class="row">
   		<div class="form-group form-inline">
-  			<div class="col-xs-12 col-sm-6 col-md-8">
+  			<div class="col-xs-12 col-sm-6 col-md-12">
   					<label id="name-id">Enter the name:</label>
                       <input type="text" class="form-control" id="name">
                        <button  align="center "type="submit" class="btn btn-primary">Add</button>
