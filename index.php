df3a871fc8c810c18f50ff337d59bd27f293ca1e
jayesh10 <jayesh.mali@somaiya.edu>
2016-12-07 21:31:55 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
   <body>
 <div class="container-fluid">
  	<div class="row">
- 		 <div class="col-xs-12 col-sm-6 col-md-8">.col-xs-12 .col-sm-6 .col-md-8</div>
-		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+ 		 <div class="col-xs-12 col-sm-6 col-md-8"></div>
 	</div>
 </div>
   </body>
